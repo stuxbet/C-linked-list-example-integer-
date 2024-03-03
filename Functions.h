decff3ad4b3b665d9b1d2e0c61a4baca7ee70b9d
stuxbet <123492671+stuxbet@users.noreply.github.com>
2024-03-03 09:16:31 -0600

    Delete Functions.h

--- Functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-
-using namespace std;
-
-void shellSort(vector<int>& invec);
-void mergeSort(vector<int>& array);
-void mergesort(vector<int>& array, vector<int>& result, int);
-void merge(vector<int>& array, vector<int>& result, int lowPointer, int highPointer, int upperBound);
